1c2e023651254368dc33e67e01e220e534dd2e73
M3a4x <38668201+M3a4x@users.noreply.github.com>
2018-07-03 15:46:58 +0300

    Update dark.js

--- dark.js
+++ dark.js
@@ -3320,4 +3320,13 @@ if (message.content.startsWith(prefix + 'الصورة')) {
 }
 });
 
+if (message.content === '*own') {
+         let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)    
+      .addField("** مبرمج البوت :wrench: **","**๓Зค4x | モアズ | مــعــاذ#9297  **")
+.setColor('RANDOM')
+  message.author.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
